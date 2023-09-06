b92f0717c6459e8587c365acfbbd9dcecb76d6a8
Troy <eremtroy@gmail.com>
2023-09-06 19:46:54 +0200

    feat: refactor tests to be more thorough

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,92 +1,133 @@
 const dbjs = require('../src/db')
 const { randomUUID } = require('crypto')
 
-describe('db.js', () => {
-  describe('db = new dbjs()', () => {
-    it('initializes', async () => {
-      const db = new dbjs()
-      expect(db).toBeInstanceOf(dbjs)
-    })
-    it('is emtpy', async () => {
-      const db = new dbjs()
-      expect(await db.get()).toEqual([])
-    })
+// Mock data
+// tests all types in truthy/falsy versions
+// total 8 items
+const data = [
+  // 4 strings
+  { value: 'example' },
+  { value: 'example two' },
+  { value: 'third example' },
+  { value: '' },
+  // 2 numbers
+  { value: 1 },
+  { value: 0 },
+  // 2 booleans
+  { value: true },
+  { value: false },
+]
+
+describe('db = new dbjs()', () => {
+  it('is instanceof dbjs', async () => {
+    const db = new dbjs()
+    expect(db).toBeInstanceOf(dbjs)
   })
 
-  describe('db.get', () => {
-    it('gets all', async () => {
-      const db = new dbjs()
-      const count = 3
-      for (let i = 0; i < count; i++) await db.set()
-      expect(await db.get()).toHaveLength(count)
-    })
+  it('is created empty', async () => {
+    const db = new dbjs()
+    expect(await db.get()).toHaveLength(0)
+  })
+})
 
-    it('gets one', async () => {
-      const db = new dbjs()
-      const { id } = await db.set()
-      expect(await db.get(id)).toHaveProperty('id', id)
-    })
-
-    it('gets none', async () => {
-      const db = new dbjs()
-      expect(await db.get('undefined-id')).toBeUndefined()
-    })
-
-    it('gets exact match from string', async () => {
-      const db = new dbjs()
-      await db.set(undefined, { value: 'example' })
-      await db.set(undefined, { value: 'another example' })
-      const result = await db.get(undefined, { value: 'example' })
-      expect(result).toHaveLength(1)
-    })
-
-    it('gets partial match from RegExp', async () => {
-      const db = new dbjs()
-      await db.set(undefined, { value: 'example' })
-      await db.set(undefined, { value: 'another example' })
-      expect(await db.get(undefined, { value: /example/ })).toHaveLength(2)
-      expect(await db.get(undefined, { value: /^example/ })).toHaveLength(1)
-    })
+describe('db.get', () => {
+  let db
+  beforeEach(async () => {
+    db = new dbjs()
+    await Promise.all(data.map(obj => db.set(undefined, obj)))
   })
 
-  describe('db.set', () => {
-    it('creates new', async () => {
-      const db = new dbjs()
-      const data = { value: 'example' }
-      const result = await db.set(undefined, data)
-      expect(result).toHaveProperty('id')
-      expect(result).toHaveProperty('created')
-      expect(result).toHaveProperty('value', data.value)
-      expect(await db.get()).toHaveLength(1)
-      await db.set(undefined, data)
-      expect(await db.get()).toHaveLength(2)
-    })
-
-    it('updates existing', async () => {
-      const db = new dbjs()
-      const created = await db.set(undefined, { value: 'example' })
-      const updated = await db.set(created.id, { value: 'updated' })
-      expect(updated).toHaveProperty('id')
-      expect(updated).toHaveProperty('created')
-      expect(updated).toHaveProperty('value', 'updated')
-      expect(await db.get()).toHaveLength(1)
-    })
+  it('db.get() gets all', async () => {
+    expect(await db.get()).toHaveLength(8)
   })
 
-  describe('db.del', () => {
-    it('deletes', async () => {
-      const db = new dbjs()
-      const { id } = await db.set()
-      expect(await db.del(id)).toEqual(id)
-      expect(await db.get(id)).toBeUndefined()
-    })
-    it('is undefined if key not found', async () => {
-      const db = new dbjs()
-      expect(await db.del(randomUUID())).toBeUndefined()
-    })
-    it('throws without key', async () => {
-      const db = new dbjs()
-      expect(async () => { await db.del() }).rejects.toThrow()
-    })
+  it('db.get(key) gets one', async () => {
+    const [first] = await db.get()
+    expect(await db.get(first.id)).toEqual(first)
+  })
+
+  it('db.get(\'undefined-key\') gets none', async () => {
+    expect(await db.get('undefined-key')).toBeUndefined()
+  })
+
+  it('db.get(undefined, { value: string })', async () => {
+    expect(await db.get(undefined, { value: 'example' })).toHaveLength(1)
+    expect(await db.get(undefined, { value: '' })).toHaveLength(1)
+  })
+
+  it('db.get(undefined, { value: number })', async () => {
+    expect(await db.get(undefined, { value: 1 })).toHaveLength(1)
+    expect(await db.get(undefined, { value: 0 })).toHaveLength(1)
+  })
+
+  it('db.get(undefined, { value: boolean })', async () => {
+    expect(await db.get(undefined, { value: true })).toHaveLength(1)
+    expect(await db.get(undefined, { value: false })).toHaveLength(1)
+  })
+
+  it('db.get(undefined, { value: RegExp })', async () => {
+    // 3 partial
+    expect(await db.get(undefined, { value: /example/ })).toHaveLength(3)
+    // 2 starging with
+    expect(await db.get(undefined, { value: /^example/ })).toHaveLength(2)
+    // 2 ending with
+    expect(await db.get(undefined, { value: /example$/ })).toHaveLength(2)
+    // 1 exact
+    expect(await db.get(undefined, { value: /^example$/ })).toHaveLength(1)
+  })
+})
+
+
+describe('db.set', () => {
+  let db
+  beforeEach(() => {
+    db = new dbjs()
+  })
+
+  it('db.set() creates with { id, created }', async () => {
+    const result = await db.set()
+    const { id, created } = result
+    expect(result).toEqual({ id, created })
+    expect(await db.get()).toHaveLength(1)
+  })
+
+  it('db.set(undefined, { value }) creates with { ..., value }', async () => {
+    const data = { value: 'example' }
+    const result = await db.set(undefined, data)
+    const { id, created, value } = result
+    expect(result).toEqual({ id, created, value })
+  })
+
+  it('db.set(key, { value }) updates existing', async () => {
+    const result = await db.set(undefined, { value: 'example' })
+    const { id, created } = result
+
+    const updated = await db.set(id, { value: 'updated' })
+    const { value } = updated
+
+    expect(updated).toEqual({ id, created, value })
+    // make sure it doesn't create another
+    expect(await db.get()).toHaveLength(1)
+  })
+})
+
+describe('db.del', () => {
+  let db
+  beforeEach(() => {
+    db = new dbjs()
+  })
+
+  it('db.del(key) deletes one', async () => {
+    const { id } = await db.set()
+    expect(await db.del(id)).toEqual(id)
+    expect(await db.get(id)).toBeUndefined()
+  })
+
+  it('db.del(\'undefined-key\') deletes none', async () => {
+    expect(await db.del('undefined-key')).toBeUndefined()
+  })
+
+  it('db.del() throws', async () => {
+    expect(async () => { await db.del() }).rejects.toThrow()
   })
 })
